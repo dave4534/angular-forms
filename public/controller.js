9dc3faf6a4b0328cbcc1c99810d23089b94ef31a
Dave <uDave@Main.local>
2016-12-22 15:16:05 +0200

    First committ

--- public/controller.js
+++ public/controller.js
@@ -1,11 +1,17 @@
 app.controller('controller', function($scope, taskService){
 $scope.tasks = taskService.tasks;
 
+
  $scope.addTask = function(){
   // alert('Hello');
   $scope.tasks.push($scope.input);
   console.log($scope.todo);
 };
+
+$scope.printUser = function(user){
+  console.log(user);
+};
+
 
 
 });
